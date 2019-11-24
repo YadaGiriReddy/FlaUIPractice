6ae0e2f258f1382fe615891645a1fcd86af683a2
Yadagiri Reddy <hyadagirireddy@gmail.com>
2019-11-24 21:56:51 +0530

    Adding test windows application for practice

---
+++ FlaUIPractice/FlaUiTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FlaUiTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Oracle Corporation")]
+[assembly: AssemblyProduct("FlaUiTests")]
+[assembly: AssemblyCopyright("Copyright © Oracle Corporation 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bcb9e6b8-8ccc-4ca4-87ca-acc39f9dfa57")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
