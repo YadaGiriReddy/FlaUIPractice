677b53c848be1845ad2786dd75abc6ab16db0eaa
Yadagiri Reddy <hyadagirireddy@gmail.com>
2020-01-17 23:09:47 +0530

    TestCaptureMethod added

--- FlaUIPractice/FlaUiTests/UnitTest1.cs
+++ FlaUIPractice/FlaUiTests/UnitTest1.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
+using FlaUI.Core;
 using FlaUI.Core.Conditions;
+using FlaUI.Core.Definitions;
 using FlaUI.Core.Input;
 using FlaUI.Core.Shapes;
+using FlaUI.Core.WindowsAPI;
+using FlaUI.UIA2;
 using FlaUI.UIA3;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -46,7 +51,7 @@ namespace FlaUiTests
             var mainWindow = application.GetMainWindow(automation);
             ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
 
-            var elements = mainWindow.FindAll(FlaUI.Core.Definitions.TreeScope.Children, 
+            var elements = mainWindow.FindAll(FlaUI.Core.Definitions.TreeScope.Children,
                 new PropertyCondition(automation.PropertyLibrary.Element.ControlType, FlaUI.Core.Definitions.ControlType.Edit));
             foreach (var item in elements)
             {
@@ -99,6 +104,50 @@ namespace FlaUiTests
             Mouse.MoveBy(100, 100);
             Mouse.LeftClick();
             Mouse.Click(MouseButton.Left, point);
+        }
+
+        [TestMethod]
+        public void TestkeyboardActions()
+        {
+            var application = FlaUI.Core.Application.Launch(@"notepad.exe");
+            var automation = new UIA3Automation();
+            var mainWindow = application.GetMainWindow(automation);
+            ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+            mainWindow.FindFirstDescendant(cf.ByControlType(FlaUI.Core.Definitions.ControlType.Document)).Focus();
+
+            Keyboard.Type('A');
+            Keyboard.Type("Hello");
+            Keyboard.Type(VirtualKeyShort.KEY_B, VirtualKeyShort.KEY_C);
+
+            Keyboard.Press(VirtualKeyShort.BACK);
+
+            Keyboard.TypeSimultaneously(VirtualKeyShort.CONTROL, VirtualKeyShort.KEY_A);
+
+            using (Keyboard.Pressing(VirtualKeyShort.SHIFT))
+            {
+                Keyboard.Type("world");
+            }
+        }
+
+        [TestMethod]
+        public void TestCaptureMethod()
+        {
+            //Full screen
+            var fullscreenImg = Capture.Screen();
+            fullscreenImg.ToFile(@"C:\Data\Visual Studio Workspace\Screenshots\Full Screen.png");
+            
+            //only one automation element
+            var application = FlaUI.Core.Application.Launch(@"C:\Data\Visual Studio Workspace\FlaUiPractice\FlaUIPractice\BankSystem\bin\Release\BankSystem.exe");
+            var automation = new UIA3Automation();
+            var mainWindow = application.GetMainWindow(automation);
+            ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+            var loginBtn = mainWindow.FindFirstDescendant(cf.ByName("Log In"));
+            var loginImg = Capture.Element(loginBtn);
+            loginImg.ToFile(@"C:\Data\Visual Studio Workspace\Screenshots\Login Button.png");
+
+            //user defined rectangle area 
+            var rectangleImg = Capture.Rectangle(new Rectangle(500, 500, 100, 150));
+            rectangleImg.ToFile(@"C:\Data\Visual Studio Workspace\Screenshots\Rectangle Img.png");
         }
     }
 }
